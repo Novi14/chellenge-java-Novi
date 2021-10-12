4b553ff2ed3e6ae410cd9b9f301b284f00446937
Novi <santikanovi220@gmail.com>
2021-10-11 22:43:36 -0700

    commit pertama

---
+++ Challenge.java
@@ -0,0 +1,25 @@
+public class Challenge {
+     public static void main(String[] args) {
+         // int number = 1234;
+         //System.out.println(firstName + number);
+         String firstName = "Novi";
+         String lastName = "Santika";
+         String age = "20";
+         String address = "Desa Lalang";
+         String phoneNumber = "082294001793";
+         String hobby = "Masak";
+         String makanKesukaan = "mie ayam";
+         String weight = "52 kg";
+         String heigth = "155 cm";        
+         System.out.println("first name;" + firstName);
+         System.out.println("last name;" + lastName);
+         System.out.println("Age;" + age);
+         System.out.println("address;" + address);
+         System.out.println("phone number;" + phoneNumber);
+         System.out.println("hoby;" + hobby);
+         System.out.println("makanan kesukaan;" + makanKesukaan);
+         System.out.println("weight;" + weight);
+         System.out.println("height;" + heigth);
+    }
+    
+}
